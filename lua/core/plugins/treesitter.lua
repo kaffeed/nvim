3662bb4001e775bed5c0b0fad0cc25adc216ead2
Sebastian Schubert <sschubert932@gmail.com>
2022-11-28 16:32:11 +0100

    don't autoinstall ts parsers

--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 require('nvim-treesitter.configs').setup({
-    ensure_installed = 'all',
+    -- ensure_installed = 'all',
     --
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
 
     -- Automatically install missing parsers when entering buffer
-    auto_install = true,
+    -- auto_install = true,
 
     -- List of parsers to ignore installing (for "all")
     ignore_install = { 'phpdoc', 'php' },
