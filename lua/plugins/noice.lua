88aeea0312e981fbb954cdccfeebdf3c5d4a3c1c
Sebastian Schubert <sebastian.schubert@inola.at>
2023-02-14 15:10:33 +0100

    disabled hover and signature

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -10,13 +10,19 @@ return {
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
                 lsp_doc_border = true, -- add a border to hover docs and signature help
             },
-            -- lsp = {
-            --   override = {
-            --     ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-            --     ['vim.lsp.util.stylize_markdown'] = true,
-            --     ['cmp.entry.get_documentation'] = true,
-            --   },
-            -- },
+            lsp = {
+                hover = {
+                    enabled = false,
+                },
+                signature = {
+                    enabled = false,
+                },
+                override = {
+                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                    ['vim.lsp.util.stylize_markdown'] = true,
+                    ['cmp.entry.get_documentation'] = true,
+                },
+            },
         },
 
         keys = {
