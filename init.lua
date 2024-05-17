8bc525bf81c03776219bb8c9905581727c60837c
s.schubert <sebastian.schubert@inola.at>
2024-05-17 10:47:35 +0200

    Switched to default neovim colorscheme

--- init.lua
+++ init.lua
@@ -263,7 +263,7 @@ require('lazy').setup {
   --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  -- { 'numToStr/Comment.nvim', opts = {} },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following lua:
@@ -757,22 +757,22 @@ require('lazy').setup {
         }
       end,
     },
-    { -- You can easily change to a different colorscheme.
-      -- Change the name of the colorscheme plugin below, and then
-      -- change the command in the config to whatever the name of that colorscheme is
-      --
-      -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-      'folke/tokyonight.nvim',
-      lazy = false, -- make sure we load this during startup if it is your main colorscheme
-      priority = 1000, -- make sure to load this before all the other start plugins
-      config = function()
-        -- Load the colorscheme here
-        vim.cmd.colorscheme 'tokyonight-night'
-
-        -- You can configure highlights by doing something like
-        vim.cmd.hi 'Comment gui=none'
-      end,
-    },
+    -- { -- You can easily change to a different colorscheme.
+    --   -- Change the name of the colorscheme plugin below, and then
+    --   -- change the command in the config to whatever the name of that colorscheme is
+    --   --
+    --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
+    --   'folke/tokyonight.nvim',
+    --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    --   priority = 1000, -- make sure to load this before all the other start plugins
+    --   config = function()
+    --     -- Load the colorscheme here
+    --     vim.cmd.colorscheme 'tokyonight-night'
+    --
+    --     -- You can configure highlights by doing something like
+    --     vim.cmd.hi 'Comment gui=none'
+    --   end,
+    -- },
 
     -- Highlight todo, notes, etc in comments
     { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
