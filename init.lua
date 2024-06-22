58f6432d9dbd702e1d9c825cb682d8e36d05d840
Sebastian Schubert <sschubert932@gmail.com>
2024-06-22 13:45:03 +0200

    Added flash

--- init.lua
+++ init.lua
@@ -800,12 +800,6 @@ require('lazy').setup {
 
         require('mini.pairs').setup()
 
-        -- Add/delete/replace surroundings (brackets, quotes, etc.)
-        --
-        -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-        -- - sd'   - [S]urround [D]elete [']quotes
-        -- - sr)'  - [S]urround [R]eplace [)] [']
-        require('mini.surround').setup()
         -- ... and there is more!
         --  Check out: https://github.com/echasnovski/mini-nvim
       end,
