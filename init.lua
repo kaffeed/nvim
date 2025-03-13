d2f12be19b530973d37572763e0a1619368cd2cb
s.schubert <sebastian.schubert@inola.at>
2025-03-13 08:09:36 +0100

    Treesitter added inc/dec selection

--- init.lua
+++ init.lua
@@ -732,6 +732,13 @@ require('lazy').setup {
           auto_install = true,
           highlight = { enable = true },
           indent = { enable = true },
+          incremental_selection = {
+            enable = true,
+            keymaps = {
+              node_incremental = 'v',
+              node_decremental = 'V',
+            },
+          },
         }
 
         -- There are additional nvim-treesitter modules that you can use to interact
